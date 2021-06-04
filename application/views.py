9eed7e4d5d97a419a3330e40b7ad54c9ab1cac73
Refref18 <refikakalyoncu@gmail.com>
2021-06-04 23:08:30 +0300

    the one everybody knows

--- application/views.py
+++ application/views.py
@@ -66,11 +66,9 @@ def login_m():
         mydb = mysql.connector.connect(
             user='root', password='Greenwich82', host='localhost', database='dtbank')
         mycursor = mydb.cursor(buffered=True)
-        print(username)
         mycursor.execute(
             "SELECT * FROM DatabaseManagers U WHERE U.username= %s", (username,))
         row = (mycursor.fetchall())
-        print(row)
         if row == []:
             return redirect(url_for('views.error'))
         if row is not []:
@@ -91,13 +89,82 @@ def login_m():
 
 @views.route('/user_page')
 def user():
+    if request.method == 'POST':
+        # RABİA KISMIIIIIIII!!!!!!!!!!!
+        if request.form.get('action1') == 'View Drug Table':
+            pass
+        if request.form.get('action2') == 'Drug Interaction Table':
+            pass
+        if request.form.get('action3') == 'Side Effect Table':
+            pass
+        if request.form.get('action4') == 'Interaction Proteins Table':
+            pass
+        if request.form.get('action5') == 'Interaction Drugs Table':
+            pass
+        if request.form.get('action6') == 'Add Contributor':
+            pass
+        if request.form.get('action7') == 'Proteins That Bind the same Drug':
+            pass
+        if request.form.get('action8') == 'Drugs with spesific side effect':
+            pass
+        if request.form.get('action9') == 'Keyword in Description':
+            pass
+        if request.form.get('action10') == 'Drugs with least side effects':
+            pass
+        if request.form.get('action11') == 'DOI of papers and contributors':
+            pass
+        if request.form.get('action12') == 'Rank Institutes':
+            pass
+        if request.form.get('action13') == 'Filter interacting targets of a specific drugs':
+            pass
     return render_template("user_page.html",  user=current_user)
 
 
-@views.route('/manager_page')
+@views.route('/manager_page', methods=['GET', 'POST'])
 def manager():
-    # Database managers shall be able to add new users to the system.
+    if request.method == 'POST':
+        # Database managers shall be able to add new users to the system.
+        if request.form.get('action1') == 'Add new user':
+            try:
+                mydb = mysql.connector.connect(
+                    host="localhost",
+                    user="root",
+                    password="Greenwich82",
+                    database='dtbank'
+                )
+                mycursor = mydb.cursor(buffered=True)
+                name = request.form.get('name')
+                username = request.form.get('username')
+                institute = request.form.get('institution')
+                password = request.form.get('password')
+                hash_pass = sha256_crypt.encrypt((password))
+                print(name, username, institute, hash_pass)
+                mycursor.execute(
+                    "INSERT INTO Users(name,username,institution,password) VALUES(%s,%s,%s,%s)", (name, username, institute, hash_pass))
+                mydb.commit()
+            except Exception:
+                print("error")
+        # Database managers shall be able to update affinity values of drugs using Reaction IDs
+        if request.form.get('action2') == 'Update affinity values':
+            pass
+        if request.form.get('action2.1') == 'Delete Drug':
+            pass
+        if request.form.get('action3') == 'Delete protein':
+            pass
+        if request.form.get('action4.1') == 'Delete Contributor':
+            pass
+        if request.form.get('action4.2') == 'Add Contributor':
+            pass
 
+    """
+     and 
+    delete drugs using DrugBank IDs. 
+    Database managers shall be able to delete proteins using UniProt IDs.
+    Database managers shall be able to update contributors of papers=documents using Reaction IDs.
+    Database managers shall be able to separately view all drugs listed in DrugBank, all proteins listed in
+    UniProt, all side eects listed in SIDER, all drug - target interactions, all papers and their contributors
+listed in BindingDB, and all users in DTBank.
+    """
     return render_template("manager_page.html",  user=current_user)
 
 
